05a292c4a177d99d54654393daf779b733498241
DineshMSDian <dinesh2742004@gmail.com>
2026-02-13 18:17:00 +0530

    SignSpeak: MediaPipe: [1/4] Implementing Mediapipe model
    Signed-off-by: DineshMSDian <dinesh2742004@gmail.com>
    
    min_detection_confidence = 0.5
        - Detection Confidence: How sure does the AI need to be to say, "Yes, that is a hand"?
    min_tracking_confidence = 0.5
        - Tracking Confidence: Once it finds the hand, how strictly should it track the movement between frames?
    
    Refernce taken by Google's official docs(archived)
    Source: https://github.com/google-ai-edge/mediapipe/blob/master/docs/solutions/hands.md

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2 
+import mediapipe as mp
 
 # Open the default camera
 camera = cv2.VideoCapture(0)
@@ -6,11 +7,25 @@ camera = cv2.VideoCapture(0)
 # Get the default frame width and height
 frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# print(f"{frame_width} x {frame_height}")
+#print(f"{frame_width} x {frame_height}")
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('first_capture.mp4', fourcc, 20.0, (frame_width, frame_height))
+
+# Initializing Mediapipe
+
+mp_hands = mp.solutions.hands                       # load ML model to track hands
+mp_drawing = mp.solutions.drawing_utils             # Visualization Helper Tool
+mp_drawing_styles = mp.solutions.drawing_styles     # (optional) for colour style
+
+# Blueprint for the model
+hands = mp_hands.Hands(
+    static_image_model = False,
+    max_num_hands = 2,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5
+)
 
 while True:
 
